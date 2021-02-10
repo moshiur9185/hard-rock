d4abc7e248589a2a5d41ce9e9579e91d143ac1df
moshiur9185 <moshiur9185@gmail.com>
2021-02-10 08:40:24 +0600

    add error massage

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ const searchSongs = async() => {
     // fetch(url)
     //     .then(res => res.json())
     //     .then(data => displaySongs(data.data))
-    const res = await fetch(url);
-    const data = await res.json();
-    displaySongs(data.data);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySongs(data.data);
+    } catch (error) {
+        displayError('something went wrong!! please try agin later...')
+    }
+
 }
 const displaySongs = songs => {
-    const songContainer = document.getElementById('song-container');
-    songs.forEach(song => {
-        // const li = document.createElement('li');
-        // li.innerText = song.title;
-        // songContainer.appendChild(li);
-        const songDiv = document.createElement('div');
-        songDiv.className = 'single-result row align-items-center my-3 p-3';
-        songDiv.innerHTML = ` 
+        const songContainer = document.getElementById('song-container');
+        songs.forEach(song => {
+            // const li = document.createElement('li');
+            // li.innerText = song.title;
+            // songContainer.appendChild(li);
+            const songDiv = document.createElement('div');
+            songDiv.className = 'single-result row align-items-center my-3 p-3';
+            songDiv.innerHTML = ` 
         <div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
@@ -30,24 +35,30 @@ const displaySongs = songs => {
         <div class="col-md-3 text-md-right text-center">
             <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>`;
-        songContainer.appendChild(songDiv);
+            songContainer.appendChild(songDiv);
 
-    })
-}
-const getLyric = async(artist, title) => {
-        const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
-        const res = await fetch(url);
-        const data = await res.json();
-        displayLyrics(data.lyrics);
-
+        })
     }
-    // const getLyric = (artist, title) => {
-    //     const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => displayLyrics(data.lyrics))
-    // }
+    // const getLyric = async(artist, title) => {
+    //         const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+    //         const res = await fetch(url);
+    //         const data = await res.json();
+    //         displayLyrics(data.lyrics);
+
+//     }
+const getLyric = (artist, title) => {
+    const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLyrics(data.lyrics))
+        .catch(error => displayError('Sorry! failed to load lyrics, Please try again later...'))
+}
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics');
     lyricsDiv.innerText = lyrics;
+}
+const displayError = error => {
+    const errorTag = document.getElementById('error-massage');
+    errorTag.innerText = error;
+
 }
