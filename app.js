547045c5241b1023981695ee110b438fd720569f
moshiur9185 <moshiur9185@gmail.com>
2021-02-10 07:58:50 +0600

    hard rock

---
+++ app.js
@@ -0,0 +1,53 @@
+// const searchSongs = () => {
+const searchSongs = async() => {
+    const searchText = document.getElementById('search-box').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+
+    //load data
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => displaySongs(data.data))
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySongs(data.data);
+}
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songs.forEach(song => {
+        // const li = document.createElement('li');
+        // li.innerText = song.title;
+        // songContainer.appendChild(li);
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = ` 
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+            <source src="${song.preview}" type="audio/mpeg"
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
+
+    })
+}
+const getLyric = async(artist, title) => {
+        const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+
+    }
+    // const getLyric = (artist, title) => {
+    //     const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => displayLyrics(data.lyrics))
+    // }
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics;
+}
